30afe07e6628eae8e915b40ae5c74beba95f0c21
Balamurugan M <m.balamuruganmdu5@gmail.com>
2022-09-24 08:21:04 +0530

    jdbc second update

--- Zoho_Training/src/com/bm/framework/jdbc/JdbcBeginner.java
+++ Zoho_Training/src/com/bm/framework/jdbc/JdbcBeginner.java
@@ -5,8 +5,15 @@ package com.bm.framework.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Properties;
+import com.bm.util.*;
 /**
  * @author Balamurugan
  *
@@ -15,47 +22,205 @@ public class JdbcBeginner
 {
 	//TODO :-1 create table method 
 	// Exp - Columns names
-	public void createTable()
+
+	public enum Creds
 	{
-		String CreateTableSQL = "create table EMPLOYEE(\r\n"+
-				 "EMPLOYEE_ID int(3) primary key,\r\n"+
-				 "NAME varchar(30) ,\r\n"+
-				 "MOBILE int(12),\r\n"+
-				 "EMAIL varchar(40),\r\n"+
-				 "DEPARTMENT varchar(30),\r\n"+");";
-		try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/incubationDB?useSSL=false", "root", "root");
-				Statement statement = connection.createStatement();){
-			
-			statement.execute(CreateTableSQL);
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		INSTANCE;
+		private Properties credentials = new Properties();
+
+		public String getCredentials(String key) {
+			return (String)credentials.get(key);
+		}
+
+		public void setCredentials(String key,String value) {
+			this.credentials.setProperty(key, value);
+		}
+		Creds()
+		{
+			setCredentials("url","jdbc:mysql://localhost:3306/incubationDB");
+			setCredentials("username","root");
+			setCredentials("password","Root@123");
 		}
 	}
-	
+	public static void createTable() throws CustomException
+	{
+
+		String createTableSql = "CREATE TABLE employee(emp_id int PRIMARY KEY,name varchar(30),mobile varchar(12),email varchar(40),department varchar(30));";
+		executeQuery(createTableSql);
+
+	}
+	public static Object executeQuery(String query) throws CustomException
+	{
+		try(Connection connection = DriverManager.getConnection(Creds.INSTANCE.getCredentials("url"),Creds.INSTANCE.getCredentials("username") ,Creds.INSTANCE.getCredentials("password") );
+				Statement statement = connection.createStatement();)
+		{			
+			boolean rescode = statement.execute(query);
+			System.out.println("Query executed successfully");
+			if(rescode)
+			{
+				ResultSet result = statement.getResultSet();
+
+				LinkedHashMap<Integer,List<Object>> db = new LinkedHashMap<>();
+				while(result.next())
+				{
+					List<Object> record = new ArrayList<Object>();
+					record.add(result.getInt(1));
+					record.add(result.getString(2));
+					record.add(result.getString(3));
+					record.add(result.getString(4));
+					record.add(result.getString(5));
+					db.put(result.getInt(1),record);
+				}
+				return db;
+			}			
+		}
+		catch (SQLException  e) 
+		{
+			throw new CustomException(e.getMessage());
+		}
+		return null;
+	}
+	//TODO Complete the show columns
+	public Object showColumns() throws CustomException
+	{
+		String createTableSql = "SHOW COLUMNS from employee";
+		//Map<I,L> mapObject = (Map<I, L>) ExecuteQuery(createTableSql);
+		return executeQuery(createTableSql);		
+	}
+
 	//TODO :-2 Add 10 employee records 
-	//
-	
+	public static void addEntry(int empId,String empName,String empMobile,String empEmail,String empDepartment) throws CustomException
+	{
+		String query = "INSERT INTO employee VALUES(?,?,?,?,?);";
+		try(Connection connection = DriverManager.getConnection(Creds.INSTANCE.getCredentials("url"),Creds.INSTANCE.getCredentials("username") ,Creds.INSTANCE.getCredentials("password") );
+				PreparedStatement prepareStatement = connection.prepareStatement(query);
+				)
+		{
+
+			prepareStatement.setInt(1, empId);
+			prepareStatement.setString(2, empName);
+			prepareStatement.setString(3, empMobile);
+			prepareStatement.setString(4, empEmail);
+			prepareStatement.setString(5, empDepartment);
+			System.out.println(prepareStatement);
+			prepareStatement.execute();
+
+		} catch (SQLException e) {
+
+			throw new CustomException(e.getMessage());			
+		}
+	}
+
 	//TODO :-3 Get the Record for Given Employee Name
-	
-	
+	public static Object getRecord(String name) throws CustomException
+	{
+		String query = "select * from employee where name=?;";
+		try(Connection connection = DriverManager.getConnection(Creds.INSTANCE.getCredentials("url"),Creds.INSTANCE.getCredentials("username") ,Creds.INSTANCE.getCredentials("password") );
+				PreparedStatement prepareStatement = connection.prepareStatement(query);
+				)
+		{
+			prepareStatement.setString(1, name);
+			System.out.println(prepareStatement);
+			boolean rescode = prepareStatement.execute();
+			if(rescode)
+			{
+				ResultSet result = prepareStatement.getResultSet();
+
+				LinkedHashMap<Integer,List<Object>> db = new LinkedHashMap<>();
+				while(result.next())
+				{
+					List<Object> record = new ArrayList<Object>();
+					record.add(result.getInt(1));
+					record.add(result.getString(2));
+					record.add(result.getString(3));
+					record.add(result.getString(4));
+					record.add(result.getString(5));
+					db.put(result.getInt(1),record);
+				}
+				return db;
+			}	
+			//			if(rescode)
+			//			{
+			//				ResultSet result = prepareStatement.getResultSet();
+			//				result.next();
+			//				System.out.println("| EMP ID |  EMP NAME  | EMP MOBILE |   EMP MAILID   | EMP DEPARTMENT | ");
+			//				System.out.format("%8s | %10s | %10s | %15s | %10s |\n",result.getInt(1),result.getString(2),result.getString(3),result.getString(4),result.getString(5));
+			//			}
+
+		} catch (SQLException e) {
+
+			throw new CustomException(e.getMessage());
+		}
+		return null;
+	}
 	//TODO 4 update the Values of an Employee
-	
+	public static void updateRecords(int empId,String key,String value) throws CustomException
+	{
+		String query = "UPDATE employee SET "+key+"=? WHERE emp_id =?;";
+		try(Connection connection = DriverManager.getConnection(Creds.INSTANCE.getCredentials("url"),Creds.INSTANCE.getCredentials("username") ,Creds.INSTANCE.getCredentials("password") );
+				PreparedStatement prepareStatement = connection.prepareStatement(query);
+				)
+		{
+			//prepareStatement.setString(1, key);
+			prepareStatement.setString(1, value);
+			prepareStatement.setInt(2,empId);
+			System.out.println(prepareStatement);
+			prepareStatement.execute();
+			getNRecords(100);
+			
+		} catch (SQLException e) {
+			
+			throw new CustomException(e.getMessage());
+		}
+	}
 	//TODO 5 Get all details of First N
-	
+	public static Object getNRecords(int nRows) throws CustomException
+	{
+		String query = "SELECT * FROM employee LIMIT ?;";
+		try(Connection connect = DriverManager.getConnection(Creds.INSTANCE.getCredentials("url"),Creds.INSTANCE.getCredentials("username"),Creds.INSTANCE.getCredentials("password"));
+				PreparedStatement prepareStatement = connect.prepareStatement(query);
+				)
+		{
+			prepareStatement.setInt(1, nRows);		
+			boolean rescode = prepareStatement.execute();
+			if(rescode)
+			{
+				ResultSet result = prepareStatement.getResultSet();
+
+				LinkedHashMap<Integer,List<Object>> db = new LinkedHashMap<>();
+				while(result.next())
+				{
+					List<Object> record = new ArrayList<Object>();
+					record.add(result.getInt(1));
+					record.add(result.getString(2));
+					record.add(result.getString(3));
+					record.add(result.getString(4));
+					record.add(result.getString(5));
+					db.put(result.getInt(1),record);
+				}
+				return db;
+			}	
+
+		}
+
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new CustomException(e.getMessage());
+		}
+		return null;
+	}
 	//TODO 6 Rep 5 Sorted by asc or des on any column
-	
+
 	//TODO 7 Delete a record by given id
-	
+
 	//TODO 8 POJO for employee Rep 2,3,5,6
-	
+
 	//TODO 9 Create Dep table NAME,AGE as Foreign key
-	
+
 	//TODO 10 ADD atleast 2 Records to dep table
-	
+
 	//TODO 11 Get Depend Record
-	
+
 	//TODO 12 GET First N employees name&emp as sorted asc
 }
 
--- Zoho_Training/src/com/bm/testrunner/TestJdbc.java
+++ Zoho_Training/src/com/bm/testrunner/TestJdbc.java
@@ -2,38 +2,129 @@
  * 
  */
 package com.bm.testrunner;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import com.bm.framework.jdbc.*;
+import com.bm.util.CustomException;
 /**
  * @author Balamurugan
  *
  */
 public class TestJdbc 
 {
+	private static void printRecord(LinkedHashMap<Integer, ArrayList<String>> map)
+	{
+		for(Entry<Integer, ArrayList<String>> entry : map.entrySet())
+		{
+			System.out.println(entry.getValue());
+		}
+	}
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
 		boolean flag = true;
-		System.out.println("1.Enter to Create Database");
-		while(flag)
+		System.out.println("1.Enter to Create Table");
+		System.out.println("2.Enter N records in Employee table");
+		System.out.println("3.Get Record for a given employee name");
+		System.out.println("4.Updating values in records");
+		System.out.println("5.Fetch N records from Emp table");
+		try
 		{
-			System.out.println("Enter your choice");
-			int n = scan.nextInt();
-			switch(n)
+			while(flag)
 			{
-			case 0:
-			{
-				flag = false;
-				break;
-			}
-			case 1:
-			{
-				JdbcBeginner jdbc = new JdbcBeginner();
-				jdbc.createTable();
-				System.out.println("Created Succesfully1");
-				break;
-			}
+				System.out.println("Enter your choice");
+				int n = scan.nextInt();
+				switch(n)
+				{
+				case 0:
+				{
+					flag = false;
+					break;
+				}
+				case 1:
+				{
+
+					JdbcBeginner.createTable();
+					System.out.println("Created Succesfully1");
+					break;
+				}
+				case 2:
+				{
+					System.out.println("Enter no of records you want to add in Employee table");
+					int count = scan.nextInt();
+					for(int i=1;i<=count;i++)
+					{
+						System.out.println("Enter EmpId ");
+						int empId = scan.nextInt();
+						scan.nextLine();
+						System.out.println("Enter Emp Name");
+						String empName = scan.nextLine();
+						System.out.println("Enter Emp mobile number");
+						String empMobile = scan.nextLine();
+						System.out.println("Enter Emp mail");
+						String empEmail = scan.nextLine();
+						System.out.println("Enter Emp Dept");
+						String empDepartment = scan.nextLine();
+						JdbcBeginner.addEntry(empId,empName,empMobile,empEmail,empDepartment);
+					}
+					break;
+				}
+				case 3:
+				{
+					System.out.println("Enter name to find a complete record ");
+					String name = scan.next();
+					LinkedHashMap<Integer,ArrayList<String>> map  = (LinkedHashMap<Integer, ArrayList<String>>) JdbcBeginner.getRecord(name);
+					printRecord(map);
+					break;
+				}
+				case 4:
+				{
+					System.out.println("Enter Emp id To update the data");
+					int empId = scan.nextInt();
+					System.out.println("Enter Choice : ");
+					System.out.println("1.Modify Mobile");
+					System.out.println("2.Modify Email");
+					System.out.println("3.Modify Department");
+					int choice = scan.nextInt();
+					if(choice == 1)
+					{
+						System.out.println("Enter Mobile Number to update : ");
+						String empNum = scan.next();
+						JdbcBeginner.updateRecords(empId,"mobile",empNum);
+					}
+					if(choice == 2)
+					{
+						System.out.println("Enter new Email : ");
+						String empEmail = scan.next();
+						JdbcBeginner.updateRecords(empId, "email", empEmail);
+					}
+					
+					if(choice == 3)
+					{
+						System.out.println("Enter new Department : ");
+						String empDept = scan.next();
+						JdbcBeginner.updateRecords(empId, "department", empDept);
+					}					
+				}
+				case 5:
+				{
+					System.out.println("Fetching N records from Employee table");
+					System.out.println("Enter Number of rows to be Fetched : ");
+					int nRows = scan.nextInt();
+					LinkedHashMap<Integer,ArrayList<String>> map = (LinkedHashMap<Integer, ArrayList<String>>) JdbcBeginner.getNRecords(nRows);
+					printRecord(map);
+					break;
+				}
+				}
 			}
 		}
+		catch(CustomException e)
+		{
+			e.getMessage();
+			e.printStackTrace();
+		}
+		scan.close();
 	}
 }
