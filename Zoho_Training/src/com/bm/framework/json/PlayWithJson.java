7781f316b08c67476b14a407daea383059afcf2d
Balamurugan M <m.balamuruganmdu5@gmail.com>
2022-09-27 16:41:47 +0530

    JSON

---
+++ Zoho_Training/src/com/bm/framework/json/PlayWithJson.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package com.bm.framework.json;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import com.bm.framework.jdbc.Employee;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import com.bm.util.CustomException;
+
+/**
+ * @author Balamurugan
+ *
+ */
+public class PlayWithJson 
+{
+	public JSONObject createJson(String fileName) throws CustomException
+	{
+		JSONObject json = new JSONObject();
+		json.put("name", "Balamurugan");
+		json.put("Role", "ZohoCorp");
+		json.put("id", 3365);
+		JSONArray taskCompleted = new JSONArray();
+		taskCompleted.put(0,"CustomHashMap");
+		taskCompleted.put(1,"Unit Testing");
+		taskCompleted.put(2,"Serialization");
+		JSONArray taskIncomplete = new JSONArray();
+		taskIncomplete.put(0,"Topics To Explore");
+		taskIncomplete.put(1,"JSON");
+		taskIncomplete.put(2,"Full Revision");
+		json.put("completed tasks", taskCompleted);
+		json.put("incomplete tasks", taskIncomplete);
+		Employee employee = new Employee();
+		employee.setEmpId(0);
+		employee.setEmpName("Test");
+		employee.setEmpPhone("1234");
+		employee.setEmpEmail("test@123.com");
+		employee.setEmpDept("test");
+		json.putOnce("Employee", employee);
+		Employee jsonArray = employee;
+		try {
+			Files.write(Paths.get(fileName),json.toString().getBytes());
+			 return json;
+		} catch (IOException e) 
+		{
+			throw new CustomException("File writing failed");
+		}
+	}
+	public JSONObject parseJsonFile(String fileName) throws CustomException
+	{
+		try(FileInputStream inputStream = new FileInputStream(new File(fileName));) 
+		{
+			JSONTokener tokener = new JSONTokener(inputStream);
+			JSONObject json = new JSONObject(tokener);
+			System.out.println(json.opt("Employeeee"));
+			return json;
+		} 
+		catch (FileNotFoundException e) 
+		{
+			throw new CustomException(e.getMessage());
+		}
+		catch (IOException e1) 
+		{
+			throw new CustomException(e1.getMessage());
+		}
+	}
+}
---
+++ Zoho_Training/src/com/bm/testrunner/TestPlayWithJson.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.bm.testrunner;
+
+import org.json.JSONObject;
+
+import com.bm.framework.json.PlayWithJson;
+import com.bm.util.CustomException;
+
+/**
+ * @author Balamurugan
+ *
+ */
+public class TestPlayWithJson 
+{
+	
+	public static void main(String[] args)
+	{
+		PlayWithJson json = new PlayWithJson();
+		try
+		{
+			System.out.println(json.createJson("example.json"));
+			System.out.println(json.parseJsonFile("example.json"));
+		} 
+		catch (CustomException e)
+		{
+			e.printStackTrace();
+		}
+		
+		//json.createJson("example.json");
+		//json.parseJsonFile("example.json");
+	}
+}
